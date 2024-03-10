a7cf278af2698dfa27c6bc18cc0170ee42c069a3
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-03-10 14:59:51 +0900

    엔티티

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //이 프로젝트에서는 싱글 테이블 전략을 사용
@@ -19,6 +23,9 @@ public abstract class Item {
 
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 
 }
